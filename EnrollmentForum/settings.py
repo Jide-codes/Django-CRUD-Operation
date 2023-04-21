50414e7cd6093c93b39411ca70a95c1c3a874a4f
Jide-codes <hezekiaholajide3@gmail.com>
2023-04-21 11:30:35 -1100

    Debbuging Application

--- EnrollmentForum/settings.py
+++ EnrollmentForum/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIR =[BASE_DIR, 'static'
     # os.path.join(BASE_DIR, 'StudentData/static/')
 ]
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT= os.path.join(BASE_DIR, "asset")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
