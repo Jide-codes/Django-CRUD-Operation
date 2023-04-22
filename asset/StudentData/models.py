ffe0f6b22024ed8245543eb5f9a6d57284f535d5
Jide-codes <hezekiaholajide3@gmail.com>
2023-04-21 20:24:00 -1100

    Debbuging Application

--- asset/StudentData/models.py
+++
@@ -1,9 +0,0 @@
-import email
-from django.db import models
-from django.forms import CharField
-
-# Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=70)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=70)
